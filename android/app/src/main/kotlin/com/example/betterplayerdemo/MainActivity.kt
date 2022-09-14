752cca7cb38ecd8afb7d32589a03c638bd913279
jagmohanSB <113092736+jagmohanSB@users.noreply.github.com>
2022-09-14 14:13:15 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/betterplayerdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.betterplayerdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:better_player/better_player.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Better player demo screen'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            BetterPlayer.network(
+              "https://d292ctx4fisa48.cloudfront.net/file_library/videos/channel_original_vod_non_drm_hls/1661778485108712/1661778485108712_108712.m3u8",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
